2b269ac3dcc4fceefb727b72dc2f972b68c67260
cryptotrader2023xx@proton.me <cryptotrader2023xx@proton.me>
2023-09-23 03:09:10 +0000

    wallet

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,74 @@
-// pages/index.js
 import React, { useState } from 'react';
-import { Container, Card } from 'react-bootstrap';
+import { Container, Card, Button } from 'react-bootstrap';
 import UserDetailsInput from '../components/UserDetailsInput';
+import { WagmiConfig, useAccount, useConnect, useDisconnect } from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import config from '../wagmi/wagmiConfig'; // Adjust the import path based on your file structure
 
 function Home() {
+  // State to store user details
   const [userDetails, setUserDetails] = useState({
     username: '',
-    // Initialize additional details as needed
+    // Add more user details fields here
+  });
+
+  // Function to update user details
+  const updateUserDetails = (newUserDetails) => {
+    setUserDetails(newUserDetails);
+  };
+
+  return (
+    <WagmiConfig config={config}>
+      <Container className="bg-black text-light">
+        <Card bg="black" text="light">
+          <Card.Header>
+            <h1>User Details App</h1>
+            <WalletControls />
+          </Card.Header>
+          <Card.Body>
+            <UserDetailsInput setUserDetails={updateUserDetails} />
+            <div>
+              <h2>User Details</h2>
+              <UserData userDetails={userDetails} />
+            </div>
+          </Card.Body>
+        </Card>
+      </Container>
+    </WagmiConfig>
+  );
+}
+
+function WalletControls() {
+  const { address, isConnected } = useAccount();
+  const { connect, disconnect } = useConnect({
+    connector: new InjectedConnector(),
   });
 
   return (
-    <Container className="bg-black text-light">
-      <Card bg="black" text="light">
-        <Card.Header>
-          <h1>User Details App</h1>
-        </Card.Header>
-        <Card.Body>
-          <UserDetailsInput setUserDetails={setUserDetails} />
-          <div>
-            <h2>User Details</h2>
-            <p>Username: {userDetails.username}</p>
-            {/* Display additional user details here */}
-          </div>
-        </Card.Body>
-      </Card>
-    </Container>
+    <div>
+      {isConnected ? (
+        <Button onClick={() => disconnect()}>Disconnect Wallet</Button>
+      ) : (
+        <Button onClick={() => connect()}>Connect Wallet</Button>
+      )}
+    </div>
   );
+}
+
+function UserData({ userDetails }) {
+  const { address, isConnected } = useAccount();
+
+  if (isConnected) {
+    return (
+      <div>
+        <p>Connected to {address}</p>
+        <p>Username: {userDetails.username}</p>
+        {/* Display additional user details here */}
+      </div>
+    );
+  }
+
+  return <p>Not connected to a wallet</p>;
 }
 
 export default Home;
